6f2fb9f8b0966f1657df19f30d7286c9e92141b2
GustavoAlves04 <gustavoalcontato@gmail.com>
2023-03-10 17:19:37 -0300

    atualizacaoJS

--- script.js
+++ script.js
@@ -1,42 +1,65 @@
-const button = document.getElementById('button')
-const select = document.getElementById('select-convert')
+const button = document.getElementById("button")
+const select = document.getElementById("value-troca")
+
 const dolar = 5.20
+const euro = 5.55
+const bitcoin = 1
 
-const convertValue = () =>{
-    const inputReal = document.getElementById('value-real').value
-    const realText = document.getElementById('valor-real')
-    const valorDolar = document.getElementById('valor-dolar')
+const convertValue = () => {
+  const inputReal = document.getElementById("value-real").value
+  const realText = document.getElementById("valor-real")
+  const valorDolar = document.getElementById("valor-dolar")
 
+  realText.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputReal)
 
-    realText.innerHTML = new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-    }).format(inputReal)
+  if (select.value === "U$ Dólar Americano") {
+    valorDolar.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(inputReal / dolar)
+  }
 
-    valorDolar.innerHTML = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-    }).format(inputReal / dolar)
+  if (select.value === "€ Euro") {
+    valorDolar.innerHTML = new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "EUR",
+      }).format(inputReal / euro)
+  }
+  if (select.value === "₿ Bitcoin") {
+    valorDolar.innerHTML = new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "BTC",
+      }).format(bitcoin * inputReal)
+  }
+  
 
 }
 
-changeSelect = () => {
-    const moeda = document.getElementById('moeda')
-    const imgBandeiraEua = document.getElementById('img-bandeira-eua')
+changeCurrency = () => {
+    const currency = document.getElementById("currency")
+    const bandeira = document.getElementById("img-bandeira-eua")
 
-
-    if(select.value = '€ Euro'){
-        moeda.innerHTML = 'Euro'
-        imgBandeiraEua.src = './img/euro.png'
+    if (select.value === "€ Euro") {
+        currency.innerHTML = "Euro"
+        bandeira.src = "./img/euro.png"
     }
 
-    if(select.value = 'U$ Dólar Americano'){
-        moeda.innerHTML = 'Dólar Americano'
-        imgBandeiraEua.src = './img/eua.png'
+    if (select.value === "U$ Dólar Americano") {
+        currency.innerHTML = "Dólar Americano"
+        bandeira.src = "./img/eua.png"
     }
+
+    if (select.value === "₿ Bitcoin") {
+        currency.innerHTML = "Bitcoin"
+        bandeira.src = "./img/bitcoin.png"
+    }
+
+    convertValue()
 }
 
 
-
-button.addEventListener('click', convertValue)
-select.addEventListener('change', changeSelect)
+button.addEventListener("click", convertValue)
+select.addEventListener("change", changeCurrency)
